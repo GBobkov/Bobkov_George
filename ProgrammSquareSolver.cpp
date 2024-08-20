3eed419a325e72bf4c7f1221404cdc9288c4f209
Bobkov_George <154755828+GBobkov@users.noreply.github.com>
2024-08-20 17:59:29 +0300

    Исправлен ввод

--- ProgrammSquareSolver.cpp
+++ ProgrammSquareSolver.cpp
@@ -1,21 +1,25 @@
 # include <stdio.h>
 # include <math.h>
 # include <assert.h>
+# include <stdlib.h>
 
 const int inf_roots = -1;
 const double proximity_pozitive_null = 0.000000001;
 
 int solve_square_func(double a, double b, double c, double* x1, double* x2);
 int is_zero(double x);
-int reading_cooficients(double* a, double* b, double* c);
+bool reading_cooficients(double* a, double* b, double* c);
 
 
 int main()
 {
-    double a = 0, b = 0, c = 0;
-    printf("Write down cooficients in format a b c:\n");
 
-    reading_cooficients(&a, &b, &c);
+    printf("Write down coeficients in format a b c:\n");
+    double a = 0, b = 0, c = 0;
+
+    bool flag = reading_cooficients(&a, &b, &c);
+
+    if (flag) return 1;
 
     double x1 = 0, x2 = 0;
     int n_roots = solve_square_func(a, b, c, &x1, &x2);
@@ -26,9 +30,9 @@ int main()
                 break;
         case 1: printf("There is one root: %lg.", x1);
                 break;
-        case 2: printf("There is two roots: %lg, %lg.", x1, x2);
+        case 2: printf("There are two roots: %lg, %lg.", x1, x2);
                 break;
-        case inf_roots: printf("There is infinity roots!");
+        case inf_roots: printf("There are infinity roots!");
                 break;
         default: printf("Error: nRoots = %d", n_roots);
     }
@@ -37,7 +41,7 @@ int main()
 
 int is_zero(double x)
 {
-    return  abs(x) < proximity_pozitive_null;
+    return  fabs(x) < proximity_pozitive_null;
 }
 
 int solve_square_func(double a, double b, double c, double* x1, double* x2)
@@ -75,6 +79,7 @@ int solve_square_func(double a, double b, double c, double* x1, double* x2)
 
     if (d < 0) return 0;
     double sqrt_d = sqrt(d);
+
     *x1 = (-b - sqrt_d) / (2 * a);
     *x2 = (-b + sqrt_d) / (2 * a);
 
@@ -83,12 +88,21 @@ int solve_square_func(double a, double b, double c, double* x1, double* x2)
 
 
 
-int reading_cooficients(double* a, double* b, double* c)
+bool reading_cooficients(double* a, double* b, double* c)
 {
-    while (scanf("%lg %lg %lg", a, b, c) != 3){
+
+    while (scanf("%lg %lg %lg", a, b, c) != 3)
+    {
         printf("Wrong input! Try again!\n");
         printf("Write down cooficients in format a b c:\n");
-        while (getchar() != '\n');
+
+        int clean = getchar();
+        while (clean != '\n')
+        {
+            clean = getchar();
+            if (clean == EOF) return true;
+        }
     }
+
+    return false;
 }
-int reading_cooficients(double* a, double* b, double* c);
